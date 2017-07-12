2b1436488447b73351dd53d373e082fb7a5b86df
Thomas Walther <thomas.walther02@sap.com>
2017-07-12 17:10:45 +0200

    replaced all , by ; and added a <br />

--- hellocgi.pl
+++ hellocgi.pl
@@ -8,10 +8,11 @@ use CGI;
 my $cgi = CGI->new();
 
 # HTML-Elemente mit cgi
-print   $cgi->header,
-        $cgi->start_html ("Hello World!"),      #...
-        $cgi->h1 ("Hello rainy World!"),        #headline 1
-        $cgi->h3 ("Hello 3rd level World!"),    #headline 3
-        $cgi->b ("Hello bold World! \n \n"),     #bold text
-        $cgi->a ("Hello World!"),               #normal text
-        $cgi->end_html,
+print $cgi->header;
+print $cgi->start_html ("Hello World!");      #...
+print $cgi->h1 ("Hello rainy World!");        #headline 1
+print $cgi->h3 ("Hello 3rd level World!");    #headline 3
+print $cgi->b ("Hello bold World! <br />");   #bold text
+print $cgi->br;                               #\n
+print $cgi->a ("Hello World!");               #normal text
+print $cgi->end_html;
