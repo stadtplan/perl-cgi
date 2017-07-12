e6aef03cba15b4c78600a4bdbb693204835b4c90
Thomas Walther <thomas.walther02@sap.com>
2017-07-12 14:24:52 +0200

    comments and /n added

--- hellocgi.pl
+++ hellocgi.pl
@@ -4,14 +4,14 @@ use strict;
 use warnings FATAL => 'all';
 use CGI;
 
+# CGI-Objekt init
 my $cgi = CGI->new();
 
-
+# HTML-Elemente mit cgi
 print   $cgi->header,
-        $cgi->start_html ("Hello World!"),
-        $cgi->h1 ("Hello rainy World!"),
-        $cgi->h3 ("Hello 3rd level World!"),
-        $cgi->b ("Hello bold World!"),
-        $cgi->a ("Hello World!"),
+        $cgi->start_html ("Hello World!"),      #...
+        $cgi->h1 ("Hello rainy World!"),        #headline 1
+        $cgi->h3 ("Hello 3rd level World!"),    #headline 3
+        $cgi->b ("Hello bold World! \n \n"),     #bold text
+        $cgi->a ("Hello World!"),               #normal text
         $cgi->end_html,
-
