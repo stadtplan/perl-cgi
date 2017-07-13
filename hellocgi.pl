08b41011505921c5a5a790c4f99e9dc69b28711e
Thomas Walther <thomas.walther02@sap.com>
2017-07-13 11:06:47 +0200

    added some comments

--- hellocgi.pl
+++ hellocgi.pl
@@ -8,11 +8,11 @@ use CGI;
 my $cgi = CGI->new();
 
 # HTML-Elemente mit cgi
-print $cgi->header;
+print $cgi->header;                           #adds a html-header
 print $cgi->start_html ("Hello World!");      #...
 print $cgi->h1 ("Hello rainy World!");        #headline 1
 print $cgi->h3 ("Hello 3rd level World!");    #headline 3
 print $cgi->b ("Hello bold World! <br />");   #bold text
 print $cgi->br;                               #\n
 print $cgi->a ("Hello World!");               #normal text
-print $cgi->end_html;
+print $cgi->end_html;                         # Idon't know what it does
